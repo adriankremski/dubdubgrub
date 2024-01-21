23adc522bd71b58d4cbfe2b3295072978320fda3
Adrian Kremski <adrian@treelineinteractive.com>
2024-01-21 15:37:43 +0100

    WIP

---
+++ DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by Adrian Kremski on 04/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
